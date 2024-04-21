939690bfc93c5ea9a201fbcf4ff37170a01b5318
Arsen-kbtu <a_zhexenbiyev@kbtu.kz>
2024-04-21 21:01:29 +0500

    models

---
+++ backend/benchmark/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class CPU(models.Model):
+    name = models.CharField(max_length=100)
+
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+    def calculate_score(self):
+        return self.release_date.year + self.release_date.month* 0.5 + self.release_date.day * 0.01
+    
+class GPU(models.Model):
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+    def calculate_score(self):
+        return self.release_date.year + self.release_date.month* 0.5 + self.release_date.day * 0.01
+    
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    release_date = models.DateField()
+    developer = models.CharField(max_length=100)
+    minimum_memory = models.IntegerField()
+    recommended_memory = models.IntegerField()
+    file_size = models.FloatField()
+    minimum_cpu = models.ForeignKey(CPU, on_delete=models.CASCADE, related_name='minimum_cpu')
+    recommended_cpu = models.ForeignKey(CPU, on_delete=models.CASCADE, related_name='recommended_cpu')
+    minimum_gpu = models.ForeignKey(GPU, on_delete=models.CASCADE, related_name='minimum_gpu')
+    recommended_gpu = models.ForeignKey(GPU, on_delete=models.CASCADE, related_name='recommended_gpu')
+
+    def __str__(self):
+        return self.name
