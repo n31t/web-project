ea6319827f6329b60c0079a2308a4441903b14c9
Amir Adilov <amiradilov202@gmail.com>
2024-04-24 01:45:39 +0600

    Refactor login/logout functionality in top-bar component

--- front/src/app/top-bar/top-bar.component.ts
+++ front/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PcService } from '../pc.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
-
+  logged !: boolean;
+  constructor(private pcService: PcService) {
+    this.pcService.logged$.subscribe(logged => {
+      this.logged = logged;
+    });
+  }
+  logout() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    this.pcService.setLogged(false);
+  }
 }
