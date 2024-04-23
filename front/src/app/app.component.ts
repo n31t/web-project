a68e2e1cb6ba819cc36b20f56ba4f4ce577389f9
Amir Adilov <amiradilov202@gmail.com>
2024-04-24 00:40:28 +0600

    Add login functionality and update app component HTML

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PcService } from './pc.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,37 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'sys-front';
-  constructor(private modalService: NgbModal) {
+  logged: boolean = false;
+  username: string = '';
+  password: string = '';
+  constructor(private modalService: NgbModal, private pcService: PcService) {
   }
 
   public open(modal: any): void {
     this.modalService.open(modal);
   }
+
+  login() {
+    this.pcService.login(this.username, this.password).subscribe(
+      token => {
+        this.logged = true
+        localStorage.setItem("access", token.access);
+        localStorage.setItem("refresh", token.refresh);
+        console.log(token);
+      }
+    )
+  }
+  logout() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    this.logged = false;
+  }
+  ngOnInit() {
+    const access = localStorage.getItem("access");
+    if (access) {
+      this.logged = true;
+    }
+  }
+
+
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GameComponent } from './game/game.component';
 import { GpuComponent } from './gpu/gpu.component';
 import { CpuComponent } from './cpu/cpu.component';
 import { AuthInterceptor } from './AuthInterceptor';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -44,6 +45,7 @@ import { AuthInterceptor } from './AuthInterceptor';
     GameComponent,
     GpuComponent,
     CpuComponent,
+    LoginComponent,
   ],
   bootstrap: [
     AppComponent
