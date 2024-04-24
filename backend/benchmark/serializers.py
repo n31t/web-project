fc1ac99cc372d631c49a69a7830209f8fc8082f2
Arsen-kbtu <a_zhexenbiyev@kbtu.kz>
2024-04-24 20:48:46 +0500

    .

--- backend/benchmark/serializers.py
+++ backend/benchmark/serializers.py
@@ -57,20 +57,38 @@ class GameSerializer(serializers.Serializer):
 
 class UserPCSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    user = serializers.CharField()
-    cpu = CPUSerializer()
-    gpu = GPUSerializer()
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    cpu_id = serializers.IntegerField()
+    gpu_id = serializers.IntegerField()
     ram = serializers.IntegerField()
     storage = serializers.IntegerField()
 
     def create(self, validated_data):
-        return UserPC.objects.create(**validated_data)
+        cpu_id = validated_data.pop('cpu_id')
+        gpu_id = validated_data.pop('gpu_id')
+
+        # Получаем объекты CPU и GPU по их ID
+        cpu = CPU.objects.get(pk=cpu_id)
+        gpu = GPU.objects.get(pk=gpu_id)
+
+        # Создаем объект UserPC и сохраняем его
+        user_pc = UserPC.objects.create(cpu=cpu, gpu=gpu, **validated_data)
+        return user_pc
 
     def update(self, instance, validated_data):
         instance.user = validated_data.get('user', instance.user)
-        instance.cpu = validated_data.get('cpu', instance.cpu)
-        instance.gpu = validated_data.get('gpu', instance.gpu)
         instance.ram = validated_data.get('ram', instance.ram)
         instance.storage = validated_data.get('storage', instance.storage)
+
+        # Обновляем CPU и GPU, если предоставлены их ID
+        cpu_id = validated_data.get('cpu_id')
+        gpu_id = validated_data.get('gpu_id')
+
+        if cpu_id is not None:
+            instance.cpu = CPU.objects.get(pk=cpu_id)
+
+        if gpu_id is not None:
+            instance.gpu = GPU.objects.get(pk=gpu_id)
+
         instance.save()
         return instance
--- backend/benchmark/views/generic_v.py
+++ backend/benchmark/views/generic_v.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from benchmark.models import UserPC, CPU, GPU, Game
 from benchmark.serializers import UserPCSerializer, CPUSerializer, GPUSerializer, GameSerializer
@@ -18,6 +19,7 @@ class UserPCListCreateView(generics.ListCreateAPIView):
     # queryset = UserPC.objects.all()
     serializer_class = UserPCSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
     def get_queryset(self):
         return UserPC.objects.filter(user=self.request.user)
     def perform_create(self, serializer):
